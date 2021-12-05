e4488fcc708efd0a8b488e1dbaa7a62f1e07e44a
teqnodude <teqno@no1.mines.nu>
2021-12-05 11:15:43 +0100

    Update - Updated update_gl.sh to support the upgrade to BETA version by beta=1 setting where the upgrade notes are highlighted at the end of upgrade

--- packages/scripts/extra/update_gl.sh
+++ packages/scripts/extra/update_gl.sh
@@ -4,10 +4,18 @@ VER=1.0
 
 glroot=/glftpd
 sourcedir=$glroot/backup/sources
+beta=0
 
 #--[ Script Start ]---------------------------------------------#
 
-latest=`lynx --dump https://glftpd.io | grep "latest stable version" | cut -d ":" -f2 | sed -e 's/20[1-9][0-9].*//' -e 's/^  //' -e 's/^v//' -e 's/ //'`
+if [ $beta = 0 ]
+then
+    latest=`lynx --dump https://glftpd.io | grep "latest stable version" | cut -d ":" -f2 | sed -e 's/20[1-9][0-9].*//' -e 's/^  //' -e 's/^v//' -e 's/ //'`
+    changelog=https://glftpd.io/files/docs/UPGRADING
+else
+    latest=`lynx --dump https://glftpd.io | grep "latest version" | cut -d ":" -f2 | sed -e 's/20[1-9][0-9].*//' -e 's/^  //' -e 's/^v//' -e 's/ BETA/.*BETA/' -e 's/ //'`
+    changelog=https://glftpd.io/files/docs/UPGRADING-beta
+fi
 version=`/glftpd/bin/glftpd-full-static | grep -o "[3-6][2-4]BiT" | sed 's/BiT//'`
 [ "$version" = "32" ] && version="86"
 [ ! -d $sourcedir ] && mkdir $sourcedir
@@ -27,7 +35,8 @@ done
 cp -rf glftpd-LNX*/docs $glroot
 sed -i 's/MAXDIRLOGSIZE 10000/MAXDIRLOGSIZE 1000000/' $glroot/bin/sources/olddirclean2.c
 sed -i 's/echo "You/#echo "You/' $glroot/bin/sources/compile.sh
-cd $glroot/bin/sources && ./compile.sh 
+cd $glroot/bin/sources && ./compile.sh
 [ -e "$glroot/bin/update_perms.sh" ] && cd $glroot/bin && ./update_perms.sh
+lynx --dump $changelog | less
 
 exit 0
