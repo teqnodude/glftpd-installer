596ed7a574077c3c3afcad8e3bfdf243a36ff46b
teqnodude <teqno@no1.mines.nu>
2021-12-07 13:48:30 +0100

    Update - Updated glftpd-version_check.sh to include support for BETA versions by beta=1 setting

--- packages/scripts/extra/glftpd-version_check.sh
+++ packages/scripts/extra/glftpd-version_check.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+VER=1.0
+#--[ Settings ]-------------------------------------------------#
 
 gllog=/glftpd/ftp-data/logs/glftpd.log
-newversion=`lynx --dump https://glftpd.io | grep "latest.stable.version" | cut -d ":" -f2 | sed -e 's/20[2-9][0-9].*//' -e 's/^  //' -e 's/^v//' -e 's/ //'`
+beta=0
+
+#--[ Script Start ]---------------------------------------------#
+
+if [ $beta = 0 ]
+then
+    newversion=`lynx --dump https://glftpd.io | grep "latest stable version" | cut -d ":" -f2 | sed -e 's/20[2-9][0-9].*//' -e 's/^  //' -e 's/^v//' -e 's/ //'`
+else
+    newversion=`lynx --dump https://glftpd.io | grep "latest version" | cut -d ":" -f2 | sed -e 's/20[2-9][0-9].*//' -e 's/^  //' -e 's/^v//' -e 's/ //'`
+fi
+
 curversion=`/glftpd/bin/glftpd-full-static | grep glFTPd | sed -e 's/(.*//' -e 's/glFTPd//' -e 's/^ //' -e 's/ //'`
 
-if [ "$newversion" != "$curversion" ]
+if [[ "$newversion" != "$curversion" ]]
 then
     echo "New version available: $newversion"
     echo `date "+%a %b %e %T %Y"` GLVERSION: \"There is a new glFTPd version available: $newversion - Current version: $curversion - https://glftpd.io\" >> $gllog
