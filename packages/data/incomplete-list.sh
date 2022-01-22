1b7f547bb011cb56ac9354cf6630392236657115
teqnodude <teqno@no1.mines.nu>
2022-01-22 17:35:56 +0100

    Update

--- packages/data/incomplete-list.sh
+++ packages/data/incomplete-list.sh
@@ -61,10 +61,8 @@ do
     do
         results="`$cleanup $glroot 2>/dev/null | grep -e "^Incomplete" | tr '\"' '\n' | grep -e "$secpath" | tr -s '/' | sort`"
 
-        if [ -z "$results" ]
+        if [ ! -z "$results" ]
         then
-            echo "$secname: No incomplete releases found."
-        else
             for result in $results
             do
                 secrel=`echo $result | sed "s|$secpath||" | tr -s '/' | sed "s|$glroot||"`
