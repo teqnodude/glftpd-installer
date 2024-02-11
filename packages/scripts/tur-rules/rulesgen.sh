0f0099062365ea1390a28c64c0837b614d27f68c
teqnodude <john@blaid.se>
2024-02-12 00:30:08 +0100

    Cosmetic

--- packages/scripts/tur-rules/rulesgen.sh
+++ packages/scripts/tur-rules/rulesgen.sh
@@ -4,11 +4,11 @@ function GENERAL
 cat << EOF > site.rules
 GENERAL :
 01.0 Site does *NOT* exist! Talk about this site/channel..........................................................[DELUSER]
-01.1 All Releases Max 2 hours from PRE ...........................................................................[NUKE 5X]
-01.2 All uploads to be completed within 12 Hours .................................................................[NUKE 5X]
-01.3 Siteops have the power, don't be a fool .....................................................................[INFO]
-01.4 Affil PRE'S are exempt from ALL rules .......................................................................[INFO]
-01.5 Accounts inactive for longer than 3 months are deleted ......................................................[INFO]
+01.1 All Releases Max 2 hours from PRE............................................................................[NUKE 5X]
+01.2 All uploads to be completed within 12 Hours..................................................................[NUKE 5X]
+01.3 Siteops have the power, don't be a fool......................................................................[INFO]
+01.4 Affil PRE'S are exempt from ALL rules........................................................................[INFO]
+01.5 Accounts inactive for longer than 3 months are deleted.......................................................[INFO]
 01.6 Accounts inactive for longer than 6 months are purged........................................................[INFO]
 
 EOF
@@ -19,7 +19,7 @@ function MISC
 {
 cat << EOF >> site.rules
 Changelog :
-90.1 Rules updated 2022-04-22.....................................................................................[INFO]
+90.1 Rules updated `date +%Y-%m-%d`.....................................................................................[INFO]
 
 EOF
 sed -i 's/sections="/sections="\nChangelog:^90./' packages/scripts/tur-rules/tur-rules.sh
