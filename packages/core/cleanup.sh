d5ee0a80030eb9b9a158eab8316b78ca7fc116c3
teqnodude <john@blaid.se>
2024-02-26 15:04:11 +0100

    Fixes and updates
    Update
    - Added a warning message in cleanup script and the option to abort to prevent people from wiping their sites by mistake
    
    Fix
    - Various errors that sadly wasn't showing up in the test environment before the 11.0 push to github

--- packages/core/cleanup.sh
+++ packages/core/cleanup.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
-rm -rf /glftpd
-rm -rf packages/source
-rm -rf packages/sitewho
-rm -rf packages/sitewho
-rm -rf packages/eggdrop*
-rm -rf packages/glftpd*
-rm -rf packages/pzs-ng
-rm -f /etc/glftpd.conf
-rm -f site.rules
-rm -rf /var/spool/mail/sitebot
-rm -rf .tmp
-rm -f /etc/rsyslog.d/glftpd.conf
-killall sitebot > /dev/null 2>&1
-sleep 3
-userdel sitebot > /dev/null 2>&1
-groupdel glftpd > /dev/null 2>&1
-sed -i /glftpd/d /etc/services
 
-if [ -f "/etc/inetd.conf" ]
-then
-    sed -i /glftpd/d /etc/inetd.conf
-    killall -HUP inetd
-fi
+echo -e "This will remove everything under \e[31m/glftpd\e[0m including \e[31m/glftpd/site\e[0m and undo system changes made by the installer"
+echo -n "Are you sure you want to proceed? [Y]es [N]o, default N : " ; read cleanup
 
-sed -i /glftpd/Id /var/spool/cron/crontabs/root
-rm -f /var/spool/cron/crontabs/sitebot
+case $cleanup in
+    [Yy])
+	rm -rf /glftpd
+	rm -rf packages/source
+	rm -rf packages/sitewho
+	rm -rf packages/sitewho
+	rm -rf packages/eggdrop*
+	rm -rf packages/glftpd*
+	rm -rf packages/pzs-ng
+        rm -f /etc/glftpd.conf
+	rm -f site.rules
+        rm -rf /var/spool/mail/sitebot
+	rm -rf .tmp
+        rm -f /etc/rsyslog.d/glftpd.conf
+	killall sitebot > /dev/null 2>&1
+        sleep 3
+	userdel sitebot > /dev/null 2>&1
+        groupdel glftpd > /dev/null 2>&1
+	sed -i /glftpd/d /etc/services
 
-if [ -f "/etc/systemd/system/glftpd.socket" ]
-then
-    systemctl stop glftpd.socket
-    systemctl disable glftpd.socket >/dev/null 2>&1
-    rm -f /etc/systemd/system/glftpd*
-    systemctl daemon-reload
-    systemctl reset-failed
-fi
+        if [ -f "/etc/inetd.conf" ]
+	then
+	    sed -i /glftpd/d /etc/inetd.conf
+	    killall -HUP inetd
+        fi
+
+	sed -i /glftpd/Id /var/spool/cron/crontabs/root
+        rm -f /var/spool/cron/crontabs/sitebot
+
+	if [ -f "/etc/systemd/system/glftpd.socket" ]
+        then
+    	    systemctl stop glftpd.socket
+	    systemctl disable glftpd.socket >/dev/null 2>&1
+	    rm -f /etc/systemd/system/glftpd*
+	    systemctl daemon-reload
+	    systemctl reset-failed
+        fi
+	echo
+	echo "Cleanup completed"
+	;;
+    *)
+	echo
+	echo "Cleanup aborted"
+	;;
+esac
 
 exit 0
