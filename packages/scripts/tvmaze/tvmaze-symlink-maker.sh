62280f093f8333c408a5aece54e35d8e686c546a
teqnodude <36924638+teqnodude@users.noreply.github.com>
2024-10-07 20:07:16 +0200

    Update tvmaze-symlink-maker.sh

--- packages/scripts/tvmaze/tvmaze-symlink-maker.sh
+++ packages/scripts/tvmaze/tvmaze-symlink-maker.sh
@@ -48,7 +48,7 @@ then
 
 	for dir in `ls $glroot/site/$section | egrep -v "$exclude"`
 	do
-	    genre="`ls $glroot/site/$section/$dir | egrep -v "IMDB|COMPLETE" | grep -o "Score_.*" | sed -e 's/(.*//' -e 's/Score_[0-9].[0-9]_-_//' -e 's/Score_NA_-_//' | tr -s "_" " " | sed 's/ - / /g'`"
+	    genre="`ls $glroot/site/$section/$dir | egrep -v "IMDB|COMPLETE" | grep -o "Score_.*" | sed -e 's/(.*//' -e 's/Score_[0-9].[0-9]_-_//' -e 's/Score_[0-9]_-_//' -e 's/Score_NA_-_//' | tr -s "_" " " | sed 's/ - / /g'`"
 	    type="`ls $glroot/site/$section/$dir | egrep -v "IMDB|COMPLETE" | grep -o "(.*)" | tr -d '()'| tr -s " " "_"`"
 
 	    if [ ! -z "$genre" ]
