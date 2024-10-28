609da9bf1d2753ee43c4db1f9eed46e7125b9c63
teqnodude <36924638+teqnodude@users.noreply.github.com>
2024-10-28 14:28:54 +0100

    Update imdb-scan.sh

--- packages/extra/imdb-scan.sh
+++ packages/extra/imdb-scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VER=1.21
+VER=1.22
 #--[ Info ]-----------------------------------------------------#
 # 
 # A script that create genre symlinks for a section of movies out 
@@ -44,7 +44,7 @@ then
 	do
 	    rating="`ls $glroot/site/$section/$dir | grep IMDB | egrep -o "Score_(NA|[0-9]|[0-9].[0-9])" | cut -d "_" -f2`"
 	    year="`ls $glroot/site/$section/$dir | grep IMDB | egrep -o "([0-9][0-9][0-9][0-9])" | tr -s '()'`"
-	    genres="`ls $glroot/site/$section/$dir | grep IMDB | grep -o "Score_.*" | sed -e 's/(.*//' -e 's/Score_[0-9].[0-9]_-_//' -e 's/Score_NA_-_//' | sed -e 's/-/ /g' -e 's/Sci Fi/Sci-Fi/' -e 's/Sci/Sci-Fi/' -e 's/Sci-Fi-Fi/Sci-Fi/' -e 's/_//g'`"
+	    genres="`ls $glroot/site/$section/$dir | grep IMDB | grep -o "Score_.*" | sed -e 's/(.*//' -e 's/Score_\([0-9]\(\.[0-9]\)\?\|NA\)_-_//' | sed -e 's/-/ /g' -e 's/Sci Fi/Sci-Fi/' -e 's/Sci/Sci-Fi/' -e 's/Sci-Fi-Fi/Sci-Fi/' -e 's/_//g'`"
 	    for genre in $genres
 	    do
 		if [ ! -z "$rating" ]
