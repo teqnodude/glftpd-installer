51cb9c5000732548840f2d4624fe0c2f160790e5
teqnodude <36924638+teqnodude@users.noreply.github.com>
2026-01-22 00:55:12 +0100

    Update pzs-ng-update.sh

--- packages/extra/pzs-ng-update.sh
+++ packages/extra/pzs-ng-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VER=1.03
+VER=1.04
 #--[ Info ]-----------------------------------------------------
 #
 # pzs-ng updater by Teqno                                       
@@ -17,9 +17,9 @@ git clone https://github.com/glftpd/pzs-ng.git
 cp zsconfig.h pzs-ng/zipscript/conf
 rm -f zsconfig.h
 
-cd pzs-ng && ./configure -q && make -s && make -s install
+cd pzs-ng && ./ng-install.sh
 
 chmod u+s "$glroot/bin/cleanup"
 chmod u+s "$glroot/bin/sed"
 
-sed -i 's|$glpath/bin/libcopy.sh|# $glpath/bin/libcopy.sh|' ng-install.sh
+
